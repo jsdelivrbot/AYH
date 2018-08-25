38bb08e373b93e0c070323d10194da829d8c0b71
Lor Chtg <lorraine.chastang@gmail.com>
2018-08-25 19:24:11 +0200

    added modal

--- client/js/app.js
+++ client/js/app.js
@@ -1,10 +1,26 @@
 import { createHeroes } from "./components/heroes-all.js"
 const getHeroes = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json"
-
+// Modal's element
+const modal = document.getElementById('heroModal')
+const buttonModal = document.getElementsByClassName("my-hero")
+const closeModal = document.getElementsByClassName("close")[0]
+// event for closing the modal
+closeModal.onclick = () => modal.style.display = "none"
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none"
+  }
+}
+// Fetch heroes' data
 window.fetch(getHeroes)
 .then(res => res.json())
 .then(heroes => {
   console.log(heroes)
   const heroesAll = document.getElementById('heroes-all')
   heroesAll.innerHTML = heroes.map(createHeroes).join(' ')
+})
+.then(() => { // add event on all heroes
+  for (let i of buttonModal) {
+    i.onclick = () => modal.style.display = "block"
+  }
 })
--- client/js/components/heroes-all.js
+++ client/js/components/heroes-all.js
@@ -1,6 +1,6 @@
 export const createHeroes = (heroes) => {
   return `
-    <div id="${heroes.id}" class="heroImg">
+    <div id="${heroes.id}" class="heroImg my-hero">
 			<img src="${heroes.images.md}" alt="${heroes.name}">
 		</div>
   `
